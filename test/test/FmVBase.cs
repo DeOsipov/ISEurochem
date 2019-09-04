f032a99ea610671b6b713f246b303e0f606a218c
DeOsipov <39943741+DeOsipov@users.noreply.github.com>
2019-09-05 00:03:05 +0300

    add FormVisual class

---
+++ test/test/FmVBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class FmVBase
+    {
+    }
+    class FmVMemorandum : FmVBase
+    {
+
+    }
+    class FmVStatement : FmVBase
+    {
+
+    }
+    class FmVRequest : FmVBase
+    {
+
+    }
+    class FmVBill : FmVBase
+    {
+
+    }
+}
